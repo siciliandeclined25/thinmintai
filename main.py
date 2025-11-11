a2f9edf126aa0b458246ecea19e966efb3626c77
lucasfrias12 <lucasfriasvapple@gmail.com>
2025-11-11 08:59:27 -0600

    Made AI prompting functional

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+import gpt4all
+from flask import Flask, render_template, request
 
 # create flask app object
 app = Flask(__name__)
+
+from gpt4all import GPT4All
+
+model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf")  # downloads / loads a 4.66GB LLM
+
+ideaprompt = ""
+extraprompt = ""
 
 
 # set route for / AKA when the page is accessed
@@ -10,5 +18,17 @@ def main():
     return render_template("ai.html")
 
 
+@app.route("/prompt", methods=["POST"])
+def getPostRequest():
+    a = model.generate(
+        "Manipulate me and "
+        + request.form["mani"]
+        + " with the prompt:"
+        + request.form["prompt"],
+        max_tokens=100,
+    )
+    return a
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
